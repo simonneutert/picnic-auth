567cd940e23603f3b6442ea8551ba2e0983ca0d2
Simon Neutert <simonneutert@users.noreply.github.com>
2026-05-26 19:22:11 +0200

    adds search

--- docs/_config.js
+++ docs/_config.js
@@ -1,10 +1,12 @@
 import lume from "lume/mod.ts";
 import lumocs from "lumocs/mod.ts";
 import mermaid from "https://deno.land/x/lume_mermaid_plugin@v0.1/mod.ts";
+import pagefind from "lume/plugins/pagefind.ts";
 
 const site = lume();
 site.copy("img");
 
+site.use(pagefind(/* Options */));
 site.use(mermaid());
 site.use(lumocs());
 
